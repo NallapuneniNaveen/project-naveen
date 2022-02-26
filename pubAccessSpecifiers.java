be29b3b0efd8a77d4a09f026044336f1d2104601
NallapuneniNaveen <99788625+NallapuneniNaveen@users.noreply.github.com>
2022-02-26 15:46:45 +0530

    Add files via upload

---
+++ pubAccessSpecifiers.java
@@ -0,0 +1,11 @@
+package pack1;
+
+public class pubAccessSpecifiers {
+
+	public void display() {
+		// TODO Auto-generated method stub
+		pubAccessSpecifiers obj = new pubAccessSpecifiers(); 
+        obj.display();
+	}
+
+}
